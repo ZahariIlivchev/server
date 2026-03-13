2188893254ecc49ac6243bde73b7f532dc2cbccc
alexandrov5433 <alexandrov5433@gmail.com>
2026-03-13 11:41:22 +0100

    Custom Request Wrapper added to be able to set a custom session on the client.

---
+++ src/main/java/diploma/server/model/type/CustomSession.java
@@ -0,0 +1,28 @@
+package diploma.server.model.type;
+
+import java.util.Map;
+
+public class CustomSession {
+
+    private Map<String, Object> claims;
+    private Long user_id;
+    private String username;
+
+    public CustomSession(Map<String, Object> claims) {
+        this.claims = claims;
+        this.user_id = Long.valueOf(String.valueOf(this.claims.get("id")));
+        this.username = (String) this.claims.get("username");
+    }
+
+    public Map<String, Object> getClaims() {
+        return claims;
+    }
+
+    public Long getUserId() {
+        return user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
