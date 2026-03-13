6764ecbdf33a65f66047f2ea5f0b971e030e52a3
alexandrov5433 <alexandrov5433@gmail.com>
2026-03-13 11:39:47 +0100

    Password encryption with BCrypt added.

---
+++ src/main/java/diploma/server/security/Bcrypt.java
@@ -0,0 +1,15 @@
+package diploma.server.security;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class Bcrypt {
+    public static String hash(String valueToHash) {
+        String hashedResult = BCrypt.withDefaults().hashToString(12, valueToHash.toCharArray());
+        return hashedResult;
+    }
+
+    public static Boolean verify(String plaintextPass, String hash) {
+        BCrypt.Result res = BCrypt.verifyer().verify(plaintextPass.toCharArray(), hash);
+        return res.verified;
+    }
+}
