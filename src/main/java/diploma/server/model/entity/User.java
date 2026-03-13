e094c96c821c503e6171a1b1f65449d4828e936b
alexandrov5433 <alexandrov5433@gmail.com>
2026-03-13 18:01:07 +0100

    User entity added. Table created successfully in DB.

---
+++ src/main/java/diploma/server/model/entity/User.java
@@ -0,0 +1,79 @@
+package diploma.server.model.entity;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nonnull
+    private String email;
+
+    @Nonnull
+    private String password;
+
+    @Nonnull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Nonnull
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Nonnull
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
